5087f2f1a79b7562a5aa7241ee2d6d788ade76b0
Deimos <salamancamanuel310@gmail.com>
2024-05-27 21:32:48 -0300

    Creación de Tablas Carrera,Profesor,Aula,Materia

--- backend/django/aulas/admin.py
+++ backend/django/aulas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import carrera,aula,profesor,materia
 # Register your models here.
+admin.site.register(carrera)
+admin.site.register(aula)
+admin.site.register(profesor)
+admin.site.register(materia)
--- backend/django/aulas/models.py
+++ backend/django/aulas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
 # Create your models here.
+class carrera(models.Model):
+    nombre = models.CharField(max_length=128, null= False)
+    
+class profesor(models.Model):
+    nombre = models.CharField(max_length=128, null= False)
+    apellido = models.CharField(max_length=128, null= False)
+    mostrar = models.TextField(max_length=256, null= False)
+
+class materia(models.Model):
+    nombre = models.CharField(max_length=128, null=False)
+    cant_alumnos = models.IntegerField(default=5, null=False)
+    carrera = models.ForeignKey(carrera, on_delete=models.CASCADE)
+    profesor = models.ForeignKey(profesor, on_delete=models.CASCADE)
+    
+class aula(models.Model):
+    descripcion = models.TextField(max_length=128, null=False)
+    ubicacion = models.CharField(max_length=128,null=False)
+    cant_proyector = models.IntegerField(default=0)
+    aforo = models.IntegerField(default=0)
+    es_climatizada = models.BooleanField(default=False)
+    
+
